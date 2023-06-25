491a9cf75ddb99daf2ee9254478a0583b197faa5
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-06-25 21:42:16 +0300

    added the init...

--- server/index.js
+++ server/index.js
@@ -3,3 +3,9 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+dotenv.config();
