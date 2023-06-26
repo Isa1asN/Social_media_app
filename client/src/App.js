43c4ce53695a694040aa08d5a79c344ce8654609
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-06-26 11:05:17 +0300

    basic structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
-
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import memories from './images/memories.png'
 const App = () => {
     return (
-        <div>
-            <h1>My App </h1>
-        </div>
+        <Container maxWidth='lg'>
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center"> Memories</Typography>
+                <img src={memories} alt="memories" height="90"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );  
 };
 
